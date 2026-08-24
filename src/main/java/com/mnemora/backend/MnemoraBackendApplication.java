2e68921c814d9468fd039419bc5b18a775aec32a
Sonal Gajbhiye <sonalgajbhiye2001@gmail.com>
2026-08-24 15:35:35 +0530

    Add comments

---
+++ src/main/java/com/mnemora/backend/MnemoraBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mnemora.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MnemoraBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MnemoraBackendApplication.class, args);
+	}
+
+}
